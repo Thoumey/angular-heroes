44010cdbfea01d1174752b69b79789b78eb4254c
Thoumey <thomas.borgelin@gmail.com>
2019-05-07 13:42:28 +0300

    First commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Ahri' },
+  { id: 12, name: 'Akali' },
+  { id: 13, name: 'Alistar' },
+  { id: 14, name: 'Amumu' },
+  { id: 15, name: 'Annie' },
+  { id: 16, name: 'Ashe' },
+  { id: 17, name: 'Blitzcrank' },
+  { id: 18, name: 'Brand' },
+  { id: 19, name: 'Caitlyn' },
+  { id: 20, name: 'Cassiopeia' }
+];
